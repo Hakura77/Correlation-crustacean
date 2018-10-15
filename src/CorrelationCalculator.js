2507dea474b892681c110d5938d03fa63aceabed
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-10-16 08:12:19 +1300

    Added CorrelationCalculator

---
+++ src/CorrelationCalculator.js
@@ -0,0 +1,8 @@
+class CorrelationCalculator () {
+  // Class to take two arrays of numbers
+  // calculates the correlation coefficent of the two arrays
+  constructor(newNumberArray1, newNumberArray2) {
+    return false
+  }
+  
+}
