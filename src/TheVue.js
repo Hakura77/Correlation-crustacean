ff6ae2861579919d8763911015fa56f64750a7a2
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-10-27 21:57:36 +1300

    Added Index.html and TheVue
    Created a html front-end using a vue instance to control it. No syntax errors have been found, but through testing is yet to be performed.

---
+++ src/TheVue.js
@@ -0,0 +1,90 @@
+
+function vueLoad() {
+  var theVue = new Vue ({
+    el: '#vueBox',
+    
+    data: {
+      theCorrelationCalculator: undefined,
+      theRegressionCalculator: undefined,
+      xPlaceholder: 'Enter your X values here, one per line or comma seperated',
+      yPlaceholder: 'Enter your Y values here, one per line or comma seperated',
+      xkPrompt: 'Enter your XK value here (optional)',
+      xValue: '',
+      yValue: '',
+      xkValue: '',
+      display: false,
+      correlationValues: undefined,
+      regressionValues: undefined
+      
+    },
+    
+    methods: {
+      
+      convertDataToArray: function(data) {
+        data = data.split(' ').join() // strip spaces
+        if (data.includes('\n')) { // entries were seperated with new lines
+          newData = data.split('\n')
+        } else if (data.includes(',')) { // entries were seperated with commas
+          newData = data.split(',')
+        } else { // entries were not seperated using a supported method
+          return false
+        }
+        
+        if (newData.length <= 0) { // no empty arrays, thank you
+          return false
+        }
+        
+        // check the data array contains only numbers
+        for (let item of newData) {
+          item = parseFloat(item)
+          if(item.isNaN()) {
+            return false
+          }
+        }
+        // data is now seperated into an array and contains only numbers and has data in it
+        return newData
+      },
+      
+      checkData: function(regMode = false) {
+        let newX = convertDataToArray(this.xValue)
+        let newY = convertDataToArray(this.yValue)
+        if(newX && newY) {
+          if(!regMode) {
+            return [newX, newY]
+          } else {
+            let newXK = parseFloat(this.xkValue)
+            return [newX, newY, newXK]
+          }
+        }
+        
+      },
+      
+      runCorrelation: function() {
+        // convert entered x and y data into arrays.
+        let theData = this.checkData()
+        if(typeof(this.theCorrelationCalculator) !== 'object') {
+          this.theCorrelationCalculator = new CorrelationCalculator(...theData)
+        } else {
+          this.theCorrelationCalculator.initalize(...theData)
+        }
+        this.correlationValues = this.theCorrelationCalculator.calculate()
+        this.display = 'c'
+      },
+      
+      runRegression: function() {
+        let theData = this.checkData(true)
+        if(typeof(this.theRegressionCalculator) !== 'object') {
+          this.theRegressionCalculator = new RegressionCalculator(...theData)
+        } else {
+          this.theRegressionCalculator.reInitalize(...theData)
+        }
+        this.regressionValues = this.theRegressionCalculator.calculate(theData[2])
+        this.display = 'r'
+        
+        // return false // not implimented yet
+      }
+      
+    }
+    
+  })
+}
