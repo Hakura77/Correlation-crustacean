34db0d6deb5de1008e3de7e41b85d9eca4edd254
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-10-31 20:14:33 +1300

    added display for when validation fails
    The interface now shows a message to the user when the provided data is invalid. Also removed commented out code that had been refactored into HelperFunctions

--- src/TheVue.js
+++ src/TheVue.js
@@ -20,70 +20,35 @@ function vueLoad() {
     
     methods: {
       
-      // following methods were moved to HelperFunctions as they were also needed in the react portion - reducing code duplication
- 
-    /*   convertDataToArray: function(data) {
-        data = data.split(' ').join('') // strip spaces
-        if (data.includes('\n')) { // entries were seperated with new lines
-          newData = data.split('\n')
-        } else if (data.includes(',')) { // entries were seperated with commas
-          newData = data.split(',')
-        } else { // entries were not seperated using a supported method
-          return false
-        }
-        
-        if (newData.length <= 0) { // no empty arrays, thank you
-          return false
-        }
-        
-        // check the data array contains only numbers - Slightly hacky, modifies the array in-place via parseFloat() and then checks to see if the call returned a NaN (and therefore the value passed isn't a number)
-        for (let item in newData) {
-          newData[item] = parseFloat(newData[item])
-          if(isNaN(newData[item])) {
-            return false
-          }
-        }
-        // data is now seperated into an array and contains only numbers and has data in it
-        return newData
-      },
-      
-      checkData: function(xValue, yValue, regMode = false) {
-        let newX = this.convertDataToArray(this.xValue)
-        let newY = this.convertDataToArray(this.yValue)
-        if(newX && newY) {
-          if(!regMode) {
-            return [newX, newY]
-          } else {
-            let newXK = parseFloat(this.xkValue)
-            return [newX, newY, newXK]
-          }
-        }
-        
-      }, */
-      
       runCorrelation: function() {
         // convert entered x and y data into arrays.
         let theData = HelperFunctions.checkData(this.xValue, this.yValue)
-        if(typeof(this.theCorrelationCalculator) !== 'object') {
-          this.theCorrelationCalculator = new CorrelationCalculator(...theData)
+        if(theData) {
+          if(typeof(this.theCorrelationCalculator) !== 'object') {
+            this.theCorrelationCalculator = new CorrelationCalculator(...theData)
+          } else {
+            this.theCorrelationCalculator.initalize(...theData)
+          }
+          this.correlationValues = this.theCorrelationCalculator.calculate()
+          this.display = 'c'
         } else {
-          this.theCorrelationCalculator.initalize(...theData)
+          this.display = 'warn'
         }
-        this.correlationValues = this.theCorrelationCalculator.calculate()
-        this.display = 'c'
       },
       
       runRegression: function() {
         let theData = HelperFunctions.checkData(this.xValue, this.yValue, true, this.xkValue)
-        if(typeof(this.theRegressionCalculator) !== 'object') {
-          this.theRegressionCalculator = new RegressionCalculator(...theData)
+        if (theData) {
+          if(typeof(this.theRegressionCalculator) !== 'object') {
+            this.theRegressionCalculator = new RegressionCalculator(...theData)
+          } else {
+            this.theRegressionCalculator.reInitalize(...theData)
+          }
+          this.regressionValues = this.theRegressionCalculator.calculate(theData[2])
+          this.display = 'r'
         } else {
-          this.theRegressionCalculator.reInitalize(...theData)
+          this.display = 'warn'
         }
-        this.regressionValues = this.theRegressionCalculator.calculate(theData[2])
-        this.display = 'r'
-        
-        // return false // not implimented yet
       }
       
     }
