c493d0a38582aa94d7014928dd056687225f8325
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-11-02 02:52:30 +1300

    Removed unnessisary require

--- RESTful_server.js
+++ RESTful_server.js
@@ -1,7 +1,6 @@
 var http = require('http')
 const CorrelationCalculator = require('./src/CorrelationCalculator_node.js')
 const RegressionCalculator = require('./src/RegressionCalculator_node.js')
-const HelperFunctions = require('./src/HelperFunctions.js')
 
 var server = http.createServer((req, res) => {
   function badData (err) {
